3799a52fced3918f1775edde902b79fc6c13ba43
suharaaa <suha.lyoko@hotmail.com>
2020-03-23 10:54:15 +0530

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StudentComponent } from './components/dashboard/student/student.component';
+import { OverviewComponent } from './components/dashboard/overview/overview.component';
+import { AddSComponent } from './components/dashboard/student/add-s/add-s.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'dashboard', 
+    component: DashboardComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'overview' },
+      { path: 'overview', component: OverviewComponent },
+      { path: 'student', 
+        component: StudentComponent,
+        children: [
+          { path: 'add', component: AddSComponent},
+        ] }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
