75448c0629c107f32ad7de611890b4d14f9b7749
Sewwandi Maweekumbura <49789125+sewmawee@users.noreply.github.com>
2020-03-27 22:47:28 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentComponent } from './components/dashboard/student/student.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { ResultsComponent } from './components/dashboard/results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentComponent,
+    DashboardComponent,
+    LoginComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
