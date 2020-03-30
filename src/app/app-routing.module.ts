d550cb8485ec48070f1439ba9c91a525cba45f65
Sewwandi Maweekumbura <49789125+sewmawee@users.noreply.github.com>
2020-03-31 03:18:37 +0530

    subjects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StudentComponent } from './components/dashboard/student/student.component';
+import { OverviewComponent } from './components/dashboard/overview/overview.component';
+import { AddSComponent } from './components/dashboard/student/add-s/add-s.component';
+import { StudentAttendanceComponent } from './components/dashboard/attendance/student-attendance/student-attendance.component';
+import { TeacherAttendanceComponent } from './components/dashboard/attendance/teacher-attendance/teacher-attendance.component';
+import { AttendanceComponent } from './components/dashboard/attendance/attendance.component';
+import { SubjectComponent } from './components/dashboard/subject/subject.component';
+import { AddsubComponent } from './components/dashboard/subject/addsub/addsub.component';
+import { UpdatesubComponent } from './components/dashboard/subject/updatesub/updatesub.component';
+import { DeletesubComponent } from './components/dashboard/subject/deletesub/deletesub.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'dashboard', 
+    component: DashboardComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'overview' },
+      { path: 'overview', component: OverviewComponent },
+      { path: 'student', 
+        component: StudentComponent,
+        children: [
+          { path: 'add', component: AddSComponent},
+        ] },
+      { path: 'attendance',
+        component: AttendanceComponent,
+        children: [
+          { path: 'students', component: StudentAttendanceComponent},
+          { path: 'teachers', component: TeacherAttendanceComponent},
+        ] },
+        { path: 'subject', 
+        component: SubjectComponent,
+        children: [
+          { path: 'addsub', component: AddsubComponent},
+          
+          { path: 'deletesub', component: DeletesubComponent},
+          { path: 'updatesub' , component: UpdatesubComponent}
+        ] },
+      
+         
+      
+      
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+ 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './components/dashboard/student/student.component';
@@ -8,23 +10,50 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { OverviewComponent } from './components/dashboard/overview/overview.component';
+import { AddSComponent } from './components/dashboard/student/add-s/add-s.component';
+import { MatDatepickerModule } from '@angular/material';
 import { ResultsComponent } from './components/dashboard/results/results.component';
+import { AttendanceComponent } from './components/dashboard/attendance/attendance.component';
+import { StudentAttendanceComponent } from './components/dashboard/attendance/student-attendance/student-attendance.component';
+import { TeacherAttendanceComponent } from './components/dashboard/attendance/teacher-attendance/teacher-attendance.component';
+import { SubjectComponent } from './components/dashboard/subject/subject.component';
+import { AddsubComponent } from './components/dashboard/subject/addsub/addsub.component';
+import { UpdatesubComponent } from './components/dashboard/subject/updatesub/updatesub.component';
+import { DeletesubComponent } from './components/dashboard/subject/deletesub/deletesub.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+  
     StudentComponent,
     DashboardComponent,
     LoginComponent,
-    ResultsComponent
+    OverviewComponent,
+    AddSComponent,
+    ResultsComponent,
+    AttendanceComponent,
+    StudentAttendanceComponent,
+    TeacherAttendanceComponent,
+    SubjectComponent,
+    AddsubComponent,
+    UpdatesubComponent,
+    DeletesubComponent,
+    
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
   ],
-  providers: [],
+    
+
+
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
