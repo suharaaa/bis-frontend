523fdedede82687da3e88320ed93775469ff352f
Chenuka Sumanasekara <58046625+chenukas@users.noreply.github.com>
2020-03-31 15:17:33 +0530

    Revert "Created subject component"

--- src/app/components/dashboard/overview/overview.component.ts
+++ src/app/components/dashboard/overview/overview.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { faSchool } from '@fortawesome/free-solid-svg-icons';
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons';
+import { faChalkboardTeacher } from '@fortawesome/free-solid-svg-icons';
+import { TaskErrorStateMatcher } from 'src/app/helpers/task-error-state-matcher';
+import { ErrorStateMatcher, MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-overview',
@@ -6,6 +11,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
+  faSchool = faSchool;
+  faUserGraduate =faUserGraduate;
+  faChalkboardTeacher = faChalkboardTeacher;
 
   constructor() { }
 
--- src/app/components/dashboard/student/add-s/add-s.component.ts
+++ src/app/components/dashboard/student/add-s/add-s.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import { StudentService } from 'src/app/services/student.service';
+import { ErrorStateMatcher, MatSnackBar } from '@angular/material';
+import { TaskErrorStateMatcher } from 'src/app/helpers/task-error-state-matcher';
 
 @Component({
   selector: 'app-add-s',
@@ -7,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSComponent implements OnInit {
 
+  private matcher: TaskErrorStateMatcher;
   
-  constructor() { }
+  email = new FormControl('', [Validators.required, Validators.email]);
+  
+  constructor(
+    private studentService: StudentService,
+    private snackbar: MatSnackBar
+  ) { }
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  
   ngOnInit() {
+    this.matcher = new TaskErrorStateMatcher();
   }
 
 }
