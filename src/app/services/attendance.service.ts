4b0b4192912a43ee0753a43520acb2b3c8447a44
chamodit <58078417+chamodit@users.noreply.github.com>
2020-04-03 03:51:29 +0530

    Changes

--- src/app/services/attendance.service.ts
+++ src/app/services/attendance.service.ts
@@ -1,2 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttendanceService {
+
+  constructor(
+    private http : HttpClient
+  ) { }
+
+  public saveStdAttendance(id, stdAttendanceRecord,count) {
+    return this.http.post(`${environment.apiHost}/classes/${id}/attendance`, {stdAttendanceRecord,count});
+  }
 
 
+}
