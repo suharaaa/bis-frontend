20f43a2b8d40e2ac790e86738d960ea916a45e5a
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-04-03 01:08:08 +0530

    Edited files

--- src/app/services/attendance.service.ts
+++ src/app/services/attendance.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import {  }
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,9 +9,5 @@ export class AttendanceService {
   constructor(
     private http: HttpClient
   ) { }
-}
-
-public addStdAttendance(data){
-  return this.http.post(`${environment.apiHost}/attendance/students/mark`, {date,classId,records[]});
 }
 
