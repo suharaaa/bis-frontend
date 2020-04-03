2501287c8610b0ac900b1dc8ce05a60769b40248
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-04-03 12:56:13 +0530

    Edited attendance service file

---
+++ src/app/services/attendance.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AttendanceService{
+
+    constructor(
+        private http: HttpClient
+      ) { }
+
+    public saveStdAttendance(classObject, attendanceRecord, count){
+        return this.http.post(`${environment.apiHost}/std-attendances`, {classObject, attendanceRecord, count});
+    }
+
+    public createStdAttendance(classObject, attendanceRecord, count){
+        return this.http.post(`${environment.apiHost}/std-attendances`, {classObject, attendanceRecord, count});
+    }
+
+    public viewStdAttendance(classObject){
+        return this.http.get(`${environment.apiHost}/std-attendances/${classObject}`);
+    }
+
+    public updateStdAttendance(id,classObject, attendanceRecord){
+        return this.http.put(`${environment.apiHost}/std-attendances/${id}`, {classObject, attendanceRecord});
+    }
+
+    public deleteStdAttendance(id){
+        return this.http.delete(`${environment.apiHost}/std-attendances/${id}`);
+    }
+
+    public getCount(count){
+        return this.http.post(`${environment.apiHost}/std-attendances`, {count});
+    }
+
+
+}
