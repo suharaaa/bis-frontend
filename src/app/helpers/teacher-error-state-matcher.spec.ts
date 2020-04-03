2f4e057e6c26c70f8246a7e40c7e7fdc21733b18
Ruvindi Guanthilake <ruvigunathilake@gmail.com>
2020-04-03 06:01:24 +0530

    creating and editing

---
+++ src/app/helpers/teacher-error-state-matcher.spec.ts
@@ -0,0 +1,7 @@
+import { TeacherErrorStateMatcher } from './teacher-error-state-matcher';
+
+describe('TeacherErrorStateMatcher', () => {
+  it('should create an instance', () => {
+    expect(new TeacherErrorStateMatcher()).toBeTruthy();
+  });
+});
