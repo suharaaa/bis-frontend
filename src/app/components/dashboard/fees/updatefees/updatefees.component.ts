1ee66a433737719d12fa401abd929bf467fd705c
AzfaMariyam <57381624+AzfaMariyam@users.noreply.github.com>
2020-04-04 19:49:08 +0530

    update fees component changes

--- src/app/components/dashboard/fees/updatefees/updatefees.component.ts
+++ src/app/components/dashboard/fees/updatefees/updatefees.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { FeesService } from 'src/app/services/fees.service';
+import {MatTableDataSource} from '@angular/material/table';
+
+interface APIResponse {
+  success : boolean,
+  data : any
+}
+
+
+
 
 @Component({
   selector: 'app-updatefees',
@@ -7,9 +18,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatefeesComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['grade', 'termfee', 'facilityfee', 'librarycharges', 'laboratorycharges', 'transportationfee', 'other'];
+  dataSource = new MatTableDataSource();
+
+  constructor(
+
+    private feesService: FeesService,
+    
+    //private feesService: FeesService,
+    private snackBar: MatSnackBar
+
+  ) { }
 
   ngOnInit() {
+
+    this.findFees();
+
+
   }
+
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  findFees(){
+    this.feesService.findFees().subscribe((res: any) => {
+      this.dataSource = res.data;
+    }, err => {
+      console.log(err.message);
+    });
+  }
+
 
 }
