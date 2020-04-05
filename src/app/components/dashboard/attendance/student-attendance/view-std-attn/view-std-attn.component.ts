75c5e24c6285dad0be5ee9688dfef52fa45fb348
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-04-05 19:09:45 +0530

    Edited files

--- src/app/components/dashboard/attendance/student-attendance/view-std-attn/view-std-attn.component.ts
+++ src/app/components/dashboard/attendance/student-attendance/view-std-attn/view-std-attn.component.ts
@@ -1,20 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { AttendanceService } from 'src/app/services/attendance.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router'
 
-export interface DailyAttendance {
-  date: string;
-  attendance: number;
+interface APIResponse {
+  success : boolean,
+  data : any
 }
 
 @Component({
   selector: 'app-view-std-attn',
   templateUrl: './view-std-attn.component.html',
   styleUrls: ['./view-std-attn.component.css']
-})
-
-@Component({
-  selector: 'table-footer-row-example',
-  styleUrls: ['./view-std-attn.component.css'],
-  templateUrl: './view-std-attn.component.html',
 })
 
 export class ViewStdAttnComponent implements OnInit {
@@ -24,26 +22,6 @@ export class ViewStdAttnComponent implements OnInit {
   ngOnInit() {
   }
 
-  displayedColumns: string[] = ['date', 'attendance'];
-  count: DailyAttendance[] = [
-    {date: '03/03/2020', attendance: 40},
-    {date: '04/03/2020', attendance: 40},
-    {date: '05/03/2020', attendance: 40},
-    {date: '06/03/2020', attendance: 37},
-  ];
-
-  getTotal() {
-    return this.count.map(c => c.attendance).reduce((acc, value) => acc + value, 0);
-  }
-
-  durations: string[] = [
-    'Daily', 'Monthly', 'Annually'
-  ];
-
-  classes: string[] = [
-    'Grade 1','Grade 2', 'Grade 3', 'Grade 4', 'Grade 5', 'Grade 6', 'Grade 7', 'Grade 8', 
-    'Grade 9', 'Grade 10', 'Grade 11', 'Grade 12', 'Grade 13'
-  ];
 }
 
 
