1db8c7aa7ad7a8a498942d97a2a61a7124f73c9e
Sewwandi Maweekumbura <49789125+sewmawee@users.noreply.github.com>
2020-04-05 21:38:29 +0530

    changes

--- src/app/components/dashboard/classes/addc/addc.component.ts
+++ src/app/components/dashboard/classes/addc/addc.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClassServices } from 'src/app/services/classes.service';
 import {  MatSnackBar } from '@angular/material';
+import { TeacherService } from 'src/app/services/teacher.service';
 
 interface APIResponse {
   success : boolean,
@@ -26,7 +27,7 @@ public teachers: [];
 
     private classService: ClassServices,
    private snackBar: MatSnackBar,
-   //public teacherService: TeacherService
+   public teacherService: TeacherService
 
   ) { }
 
@@ -35,13 +36,16 @@ public teachers: [];
     this.name ='';
     this.classteacher ='';
 
-  /* this.teacherService.findTeacher().subscribe(res =>{
-     this.teachers = res.data;
-   })
   
-*/
+  
+this.viewTeacher();
 
   }
+
+  public viewTeacher() {
+    this.teacherService.viewTeacher().subscribe((res: { data: any }) => this.teachers = res.data);
+  }
+
 
   createNewClass(){
     this.classService.createNewClass(this.name,this.classteacher).subscribe(response => {
