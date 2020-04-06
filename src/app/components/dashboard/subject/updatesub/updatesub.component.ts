8561281aa36b17d4d2cd11e5879f93897e9f8603
Sewwandi Maweekumbura <49789125+sewmawee@users.noreply.github.com>
2020-04-06 22:04:29 +0530

    changes

--- src/app/components/dashboard/subject/updatesub/updatesub.component.ts
+++ src/app/components/dashboard/subject/updatesub/updatesub.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectServices } from 'src/app/services/subject.service';
+import { ClassServices } from 'src/app/services/classes.service';
+import {  MatSnackBar } from '@angular/material';
+import {MatTableDataSource} from '@angular/material/table';
+
+interface APIResponse {
+  success :  boolean,
+  data : any
+
+}
 
 @Component({
   selector: 'app-updatesub',
@@ -7,9 +17,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatesubComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['subjectname', 'classname', 'teachername','action'];
+  dataSource = new MatTableDataSource();
 
-  ngOnInit() {
+
+  constructor(
+    private subjectServices : SubjectServices,
+    private snackBar : MatSnackBar
+
+  ) { }
+
+  ngOnInit() :void{
+ 
+    this.findSubjects();
+    
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  findSubjects(){
+    this.subjectServices.findSubjects().subscribe((res: any) => {
+      this.dataSource = res.data;
+    }, err => {
+      console.log(err.message);
+    });
+  }
+
+  DeleteSubject(id:String){
+    this.subjectServices.DeleteSubject(id).subscribe(response => {
+    console.log(response);
+   this.snackBar.open('Subjects and teacher added successfully', null, { duration : 2000});
+    }, err => {
+    this.snackBar.open('Subject name and Class required', null, { duration : 3000});
+      console.log(err.message);
+  });
+    
+    
   }
 
 }
+
+
+
