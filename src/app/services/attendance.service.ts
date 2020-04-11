bad51d9ac598a11b34220567fd768132d2629359
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-04-12 00:39:23 +0530

    Changes

--- src/app/services/attendance.service.ts
+++ src/app/services/attendance.service.ts
@@ -11,30 +11,5 @@ export class AttendanceService{
     constructor(
         private http: HttpClient
       ) { }
-
-    public createAttendance(attendanceRecord) {
-        return this.http.post(`${environment.apiHost}/attendance`, {attendanceRecord});
-    }
-
-    public viewAttendance(){
-        return this.http.get(`${environment.apiHost}/attendance`);
-    }
-
-    public viewAttendanceById(id){
-        return this.http.get(`${environment.apiHost}/attendance/${id}`);
-    }
-
-    public updateAtttendance(id, attendanceRecord){
-        return this.http.put(`${environment.apiHost}/attendance/${id}`, {attendanceRecord});
-    }
-
-    public deleteAttendance(id){
-        return this.http.delete(`${environment.apiHost}/attendance/${id}`);
-    }
-
-    public getCount(id){
-        return this.http.get(`${environment.apiHost}/attendances/${id}`);
-    }
-
     
 }
